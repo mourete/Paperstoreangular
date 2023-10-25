8ffc357101de40167a0c9820053a278d862efee1
karlagn96 <karlaivonnegn@gmail.com>
2023-10-25 09:19:38 -0600

    global-constants actualizado

--- src/app/model/global-constants.ts
+++ src/app/model/global-constants.ts
@@ -1,8 +1,8 @@
 export class GlobalConstants {
-   //public static apiURL: string = "http://paperstoredigital.net:8085/";
-   public static apiURL: string = "http://3.21.151.73:8085/";
-   //public static apiURL: string = "http://localhost:8085/";
-   
+   // public static apiURL: string = "http://paperstoredigital.net:8085/";
+   // public static apiURL: string = "http://3.21.151.73:8085/";
+   public static apiURL: string = "http://localhost:8085/";
+
     public static CONCEPTO_TIPO_TEXTO : number = 1;
     public static CONCEPTO_TIPO_ENTERO : number = 2;
     public static CONCEPTO_TIPO_DECIMAL : number = 3;
@@ -17,8 +17,8 @@ export class GlobalConstants {
     public static CONCEPTO_TIPO_MENSAJE :number=11;
     public static CONCEPTO_TIPO_VIGENCIA :number=13;
 
- 
-   
+
+
 
    public static CURRENT_DOCUMENTO_ID="CURRENT_DOCUMENTO_ID";
    public static CURRENT_PROYECTO_ID="CURRENT_PROYECTO_ID";
@@ -41,7 +41,7 @@ export class GlobalConstants {
    public static MENU_PROYECTOS="PROYECTOS";
    public static MENU_DASHBOARD = "DASHBOARD_OPERATIVO";
    public static MENU_STORECHECK = "STORECHECK";
- 
+
 
 
 }

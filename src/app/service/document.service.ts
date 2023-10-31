6445fc854d80575ad63b55be6d283bd5f9d4b2b0
Karlagn96 <72469336+Karlagn96@users.noreply.github.com>
2023-10-30 21:33:37 -0600

    Se enviaran las actividades encargadas relacionado con diseño documento (#26)
    * Arreglo documento instancia edit
    
    * ya permite guardar en diseño documento. solo que no envia la informacion correctamente.
    
    * Se cambio el nombre en document ya que estaba profileRegion y se cambio a profileDocument.
    
    Se cambio que al momento de editar aparezca el nombre que estamos apunto de editar.
    
    falta que guarde, ya que hay problemas en el back en el archivo DAODocumento
    
    * Arreglo con ts ignore

--- src/app/service/document.service.ts
+++ src/app/service/document.service.ts
@@ -17,18 +17,18 @@ export class DocumentService {
   constructor( private http : HttpClient ) { }
 
   public getDocumentTest() : Observable<Documento> {
-      let url:string = GlobalConstants.apiURL + "documentos/1"; 
+      let url:string = GlobalConstants.apiURL + "documentos/1";
       return this.http.get<Documento>( url  );
   }
 
   public getAll(usuarioOID:string) : Observable<Documento[]> {
-    let url:string = GlobalConstants.apiURL + "documentos/all"  + "/" + usuarioOID; 
+    let url:string = GlobalConstants.apiURL + "documentos/all"  + "/" + usuarioOID;
     return this.http.get<Documento[]>( url  );
 }
 
 
 public getAllActives(usuarioOID:string) : Observable<Documento[]> {
-  let url:string = GlobalConstants.apiURL + "documentos/allActives"  + "/" + usuarioOID; 
+  let url:string = GlobalConstants.apiURL + "documentos/allActives"  + "/" + usuarioOID;
   console.log(GlobalConstants.apiURL + "documentos/allActives"  + "/" + usuarioOID );
   return this.http.get<Documento[]>( url  );
 }
@@ -36,39 +36,39 @@ public getAllActives(usuarioOID:string) : Observable<Documento[]> {
 
 
 public getDocumentForConfig(documentoId:number, usuarioOID:string) : Observable<Documento> {
-    let url:string = GlobalConstants.apiURL + "documentos/design/" + documentoId + "/" + usuarioOID; 
+    let url:string = GlobalConstants.apiURL + "documentos/design/" + documentoId + "/" + usuarioOID;
     return this.http.get<Documento>( url  );
 }
- 
+
 public getDocumentoResumen(documentoId:number, usuarioOID:string) : Observable<DocumentoResumen> {
-   
-  let url:string = GlobalConstants.apiURL + "documentos/resumen/" + documentoId  + "/" + usuarioOID; 
+
+  let url:string = GlobalConstants.apiURL + "documentos/resumen/" + documentoId  + "/" + usuarioOID;
   return this.http.get<DocumentoResumen>( url  );
 }
 
 
 public getDocumentosUsuario( usuarioOID : string ) : Observable<DocumentosUsuario> {
-  let url:string = GlobalConstants.apiURL + "documentos/documentosUsuario/" + usuarioOID ; 
+  let url:string = GlobalConstants.apiURL + "documentos/documentosUsuario/" + usuarioOID ;
   return this.http.get<DocumentosUsuario>( url  );
 }
 
 public getDocumentosUsuarioList(marcaId: number , empresaId : number,  usuarioOID : string ) : Observable<ListDocumentosUsuarios[]> {
-  let url:string = GlobalConstants.apiURL + "documentos/documentosUsuarioList/" + usuarioOID  + "/"  + marcaId + "/"  + empresaId; 
+  let url:string = GlobalConstants.apiURL + "documentos/documentosUsuarioList/" + usuarioOID  + "/"  + marcaId + "/"  + empresaId;
   return this.http.get<ListDocumentosUsuarios[]>( url  );
 }
 
   public guardarDocumento(documento: Documento, usuarioOID:string ): Observable< Documento > {
-    let url:string = GlobalConstants.apiURL + "documentos/save"; 
-    return this.http.post<Documento>( url , JSON.stringify(documento), {headers: this.headers});    
-}  
+    let url:string = `${GlobalConstants.apiURL}documentos/save/${usuarioOID}`;
+    return this.http.post<Documento>( url , JSON.stringify(documento), {headers: this.headers});
+}
 
 public deleteDocumento(documentoId:number, usuarioOID:string ): Observable< string > {
-  let url:string = GlobalConstants.apiURL + "documentos/delete/" + documentoId  + "/" + usuarioOID;  
-  return this.http.post<string>( url , JSON.stringify(documentoId), {headers: this.headers});    
-}  
- 
+  let url:string = GlobalConstants.apiURL + "documentos/delete/" + documentoId  + "/" + usuarioOID;
+  return this.http.post<string>( url , JSON.stringify(documentoId), {headers: this.headers});
+}
+
 public getByDocumentoId(documentoId:number, usuarioOID:string) : Observable<Documento> {
-    let url:string = GlobalConstants.apiURL + "documentos/byDocumentoId/" + documentoId  + "/" + usuarioOID ; 
+    let url:string = GlobalConstants.apiURL + "documentos/byDocumentoId/" + documentoId  + "/" + usuarioOID ;
     return this.http.get<Documento>( url  );
 }
 
